c97ed7271ff2c858b4d30ff6d1eca8491423833d
ssp-eledia <150007115+ssp-eledia@users.noreply.github.com>
2023-11-06 14:57:34 +0100

    .

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,11 +1,12 @@
 {
     "name": "Estate",  # The name that will appear in the App list
-    "version": "16.0.22",  # Version
+    "version": "16.0.23",  # Version
     "application": True,  # This line says the module is an App, and not a module
     "depends": ["base", "web"],  # dependencies
     "data": [
         "security/ir.model.access.csv",
         "views/estate_property_views.xml",
+        "views/estate_property_type_views.xml",
         "views/estate_menus.xml",
     ],
     "installable": True,
---
+++ estate/models/estate_property_type.py
@@ -0,0 +1,5 @@
+from odoo import fields, models
+
+class EstatePropertyType(models.Model):
+    _description = "Estate Model Type"
+    _name = "estate.property.type"

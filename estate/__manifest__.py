d23f282a4b05ffe855b86824ac16fc0bd5866429
ssp-eledia <150007115+ssp-eledia@users.noreply.github.com>
2023-11-06 10:34:40 +0100

    .

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -2,9 +2,9 @@
     "name": "Estate",  # The name that will appear in the App list
     "version": "16.0",  # Version
     "application": True,  # This line says the module is an App, and not a module
-    "depends": ["base"],  # dependencies
+    "depends": ["base", "web"],  # dependencies
     "data": [
-
+        "security/ir.model.access.csv",
     ],
     "installable": True,
     'license': 'LGPL-3',

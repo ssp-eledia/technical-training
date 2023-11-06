f7db306f987d4cd2a45422b9954e59bcfab875d2
ssp-eledia <150007115+ssp-eledia@users.noreply.github.com>
2023-11-06 12:22:40 +0100

    Update __manifest__.py

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Estate",  # The name that will appear in the App list
-    "version": "16.0.11",  # Version
+    "version": "16.0.12",  # Version
     "application": True,  # This line says the module is an App, and not a module
     "depends": ["base", "web"],  # dependencies
     "data": [

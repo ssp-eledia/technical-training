66d77e3e1b03a71c1df767c81a0b135a82bb89f5
ssp-eledia <150007115+ssp-eledia@users.noreply.github.com>
2023-11-06 12:15:11 +0100

    .

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,16 +1,21 @@
+from dateutil.relativedelta import relativedelta
+
 from odoo import fields, models
 
 class EstateProperty(models.Model):
     _description = "Estate Model"
     _name = "estate.property"
 
+    def _default_date_availability(self):
+        return fields.Date.context_today(self) + relativedelta(months=3)
+
     name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date(copy=False)
+    date_availability = fields.Date(copy=False, default=lambda self: self._default_date_availability())
     expected_price = fields.Float(required=True)
     selling_price = fields.Float(readonly=True, copy=False)
-    bedrooms = fields.Integer()
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()

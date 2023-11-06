b885c80bbf421a5829cb17741c518c5a01d1af96
ssp-eledia <150007115+ssp-eledia@users.noreply.github.com>
2023-11-06 16:29:16 +0100

    .

--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -5,7 +5,7 @@ class EstatePropertyOffer(models.Model):
     _name = "estate.property.offer"
 
     price = fields.Float("Offer Price")
-    status = fields.Selection(string="Status", copy=False, selection=[('accepted', 'Accepted'), ('refused', 'Refused')])
+    state = fields.Selection(string="Status", copy=False, selection=[('accepted', 'Accepted'), ('refused', 'Refused')])
 
     partner_id = fields.Many2one("res.partner", string="Buyer", required=True)
     property_id = fields.Many2one("estate.property", string="Property", required=True)
